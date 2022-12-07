9b7d9af45043326e30928654f17cb082666a2b24
kny3705 <kkny3705@gmail.com>
2022-12-07 13:27:23 +0900

    controller에서 serviceImpl 호출 -> service 호출 변경

--- src/main/java/com/kspia/mtdservice/controller/MtdWaterLeakExamController.java
+++ src/main/java/com/kspia/mtdservice/controller/MtdWaterLeakExamController.java
@@ -1,27 +1,28 @@
 package com.kspia.mtdservice.controller;
 
 import com.kspia.mtdservice.entity.MtdWaterLeakExamGroup;
-import com.kspia.mtdservice.service.impl.GroupServiceImpl;
-import com.kspia.mtdservice.service.impl.WateruserServiceImpl;
+import com.kspia.mtdservice.service.GroupService;
+import com.kspia.mtdservice.service.WateruserService;
 import com.kspia.mtdservice.vo.RequestGroup;
 import com.kspia.mtdservice.vo.RequestWateruser;
 import com.querydsl.core.Tuple;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sm-mtd-service")
 public class MtdWaterLeakExamController {
+
     Environment env;
-    GroupServiceImpl groupService;
-    WateruserServiceImpl wateruserService;
+    GroupService groupService;
+    WateruserService wateruserService;
 
-
-
-    public MtdWaterLeakExamController(Environment env,GroupServiceImpl groupService, WateruserServiceImpl wateruserService) {
-
+    public MtdWaterLeakExamController(Environment env, GroupService groupService, WateruserService wateruserService) {
         this.env = env;
         this.groupService = groupService;
         this.wateruserService = wateruserService;
@@ -29,17 +30,17 @@ public class MtdWaterLeakExamController {
 
     @GetMapping("/health_check")
     public String status() {
-        return  String.format("It's Working in Mtd Service on PORT %s",
-                env.getProperty("local.server.port"));
-   }
+        return String.format("It's Working in Mtd Service on PORT %s",
+            env.getProperty("local.server.port"));
+    }
 
-    @RequestMapping(value = "/group/list",method = RequestMethod.POST)
+    @RequestMapping(value = "/group/list", method = RequestMethod.POST)
     public List<MtdWaterLeakExamGroup> getPostList(@RequestBody RequestGroup group) {
         List<MtdWaterLeakExamGroup> groupEntityList = groupService.getPostList(group);
         return groupEntityList;
     }
 
-    @RequestMapping(value = "/wateruser/list",method = RequestMethod.POST)
+    @RequestMapping(value = "/wateruser/list", method = RequestMethod.POST)
     public List<Tuple> getResultList(@RequestBody RequestWateruser wateruser) {
         List<Tuple> wateruserEntityList = wateruserService.getResultList(wateruser);
         return wateruserEntityList;
@@ -49,8 +50,6 @@ public class MtdWaterLeakExamController {
 //        List<ConsumerModemInfoEntity> wateruserEntityList = wateruserService.getResultList(wateruser);
 //        return wateruserEntityList;
 //    }
-
-
 
 
 }
