e39aa35687ddadd43b375ec7f86e21c0d78946ab
kny3705 <kkny3705@gmail.com>
2022-12-08 16:09:20 +0900

    대시보드 단말기 상태 조회 API 구현

---
+++ src/main/java/com/kspia/mtdservice/dto/MeterdailyDto.java
@@ -0,0 +1,50 @@
+package com.kspia.mtdservice.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @since 2022-12-08
+ * @author kkny3
+ *
+ * @Discript
+ * ---------------------------------------------------
+ * 개요 : MeterdailyDto 등 관련 Dto 클래스
+ * ---------------------------------------------------
+ * @EditHIstory
+ * 개정이력
+ * 2022-12-08 kkny3 : 최초 작성
+ * 2022-12-08 kkny3 : MeterdailyDto, MeterdailyDto.ModemCount 작업
+ */
+@Data
+@NoArgsConstructor
+public class MeterdailyDto {
+    private String modem_id;
+    private Date daily_date;
+    private BigDecimal past_value;
+    private Date metering_date;
+    private BigDecimal new_value;
+    private BigDecimal daily_usage;
+    private Integer meter_backflow;
+    private Integer meter_battery;
+    private Integer meter_overflow;
+    private Integer meter_waterleak;
+    private String  modem_rssi;
+    private BigDecimal modem_connect;
+    private Integer time_sync;
+    private String daily_tag;
+    private Date insert_date;
+    private Date update_date;
+    private BigDecimal modem_battery;
+
+    @Data
+    @NoArgsConstructor
+    public static class ModemCount {
+        private long modemLowBatteryCnt;
+        private long timeSyncCnt;
+        private long disconnectCnt;
+    }
+}
---
+++ src/main/java/com/kspia/mtdservice/repository/MeterdailyRepository.java
@@ -0,0 +1,24 @@
+package com.kspia.mtdservice.repository;
+
+import com.kspia.mtdservice.dto.MeterdailyDto;
+import com.kspia.mtdservice.dto.MeterdailyDto.ModemCount;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @since 2022-12-08
+ * @author kkny3
+ *
+ * @Discript
+ * ---------------------------------------------------
+ * 개요 : MeterdailyRepository 인터페이스
+ * ---------------------------------------------------
+ * @EditHIstory
+ * 개정이력
+ * 2022-12-08 kkny3 : 최초 작성
+ * 2022-12-08 kkny3 : countByModemStatus 작업
+ */
+@Repository
+public interface MeterdailyRepository {
+    ModemCount countByModemStatus();
+}
