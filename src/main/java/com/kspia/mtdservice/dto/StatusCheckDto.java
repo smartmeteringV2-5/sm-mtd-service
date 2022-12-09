eade6cb89cd3db981fce02f17876285c81ba7613
jung <102740808+Jung-eee@users.noreply.github.com>
2022-12-09 10:29:26 +0900

    status check dto 1

--- src/main/java/com/kspia/mtdservice/dto/StatusCheckDto.java
+++ src/main/java/com/kspia/mtdservice/dto/StatusCheckDto.java
@@ -1,4 +1,17 @@
 package com.kspia.mtdservice.dto;
-
+/**
+ * @since 2022. 12. 9
+ * @author jung
+ *
+ * @Discript
+ * ---------------------------------------------------
+ * 개요 : 실시간 현황 조회 StatusCheckDto 생성
+ * ---------------------------------------------------
+ * @EditHIstory
+ * 개정이력
+ * 2022-12.9 jung : 최초 작성
+ * 2022.12.9 jung : 단말기 카운트 작업
+ */
 public class StatusCheckDto {
+
 }
